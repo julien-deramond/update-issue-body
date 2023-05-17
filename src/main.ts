f3b8460455b4f72adef1c848492692744ebe1f40
Julien Déramond <juderamond@gmail.com>
2023-05-17 18:50:38 +0200

    .

---
+++ src/main.ts
@@ -0,0 +1,55 @@
+import * as core from '@actions/core'
+import {Inputs, updateIssueBody} from './update-issue-body'
+import {inspect} from 'util'
+import * as utils from './utils'
+
+function getBody(inputs: Inputs) {
+  if (inputs.body) {
+    return inputs.body
+  } else {
+    return ''
+  }
+}
+
+async function run(): Promise<void> {
+  try {
+    const inputs: Inputs = {
+      token: core.getInput('token'),
+      repository: core.getInput('repository'),
+      issueNumber: Number(core.getInput('issue-number')),
+      body: core.getInput('body'),
+      editMode: core.getInput('edit-mode'),
+      appendSeparator: core.getInput('append-separator'),
+    }
+    core.debug(`Inputs: ${inspect(inputs)}`)
+
+    if (!['append', 'replace'].includes(inputs.editMode)) {
+      throw new Error(`Invalid edit-mode '${inputs.editMode}'.`)
+    }
+
+    if (!['newline', 'space', 'none'].includes(inputs.appendSeparator)) {
+      throw new Error(`Invalid append-separator '${inputs.appendSeparator}'.`)
+    }
+
+    const body = getBody(inputs)
+
+    if (inputs.issueNumber) {
+      if (!body) {
+        throw new Error("Missing comment 'body'.")
+      }
+    } else {
+      throw new Error("Missing 'issue-number'.")
+    }
+
+    updateIssueBody(inputs, body)
+  } catch (error) {
+    core.debug(inspect(error))
+    const errMsg = utils.getErrorMessage(error)
+    core.setFailed(errMsg)
+    if (errMsg == 'Resource not accessible by integration') {
+      core.error(`See this action's readme for details about this error`)
+    }
+  }
+}
+
+run()
---
+++ src/update-issue-body.ts
@@ -0,0 +1,77 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+
+export interface Inputs {
+  token: string
+  repository: string
+  issueNumber: number
+  body: string
+  editMode: string
+  appendSeparator: string
+}
+
+function appendSeparatorTo(body: string, separator: string): string {
+  switch (separator) {
+    case 'newline':
+      return body + '\n'
+    case 'space':
+      return body + ' '
+    default: // none
+      return body
+  }
+}
+
+async function updateBody(
+  octokit,
+  owner: string,
+  repo: string,
+  issueNumber: number,
+  body: string,
+  editMode: string,
+  appendSeparator: string
+): Promise<number> {
+  if (body) {
+    let issueBody = ''
+    if (editMode == 'append') {
+      // Get the issue body
+      const {data: issue} = await octokit.rest.issues.get({
+        owner: owner,
+        repo: repo,
+        issue_number: issueNumber
+      })
+      issueBody = appendSeparatorTo(
+        issue.body ? issue.body : '',
+        appendSeparator
+      )
+    }
+    issueBody = issueBody + body
+    core.debug(`Issue body: ${issueBody}`)
+    await octokit.rest.issues.update({
+      owner: owner,
+      repo: repo,
+      issue_number: issueNumber,
+      body: issueBody
+    })
+    core.info(`Updated issue id '${issueNumber}'.`)
+  }
+  return issueNumber
+}
+
+export async function updateIssueBody(
+  inputs: Inputs,
+  body: string
+): Promise<void> {
+  const [owner, repo] = inputs.repository.split('/')
+
+  const octokit = github.getOctokit(inputs.token)
+
+  await updateBody(
+    octokit,
+    owner,
+    repo,
+    inputs.issueNumber,
+    body,
+    inputs.editMode,
+    inputs.appendSeparator
+  )
+}
